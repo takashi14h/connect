4be22b4f5b8add37ed73d12f644de4ac8221d3ea
takashi14h <th14snowboarding@gmail.com>
2018-10-09 10:59:32 +0000

    ログイン機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+def after_sign_out_path_for(resource)
+    new_user_session_path # ログアウト後に遷移するpathを設定
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'tops#top'
+  
+  # get 'catalogs/new'
+  # get 'catalogs/index'
+  # get 'catalogs/show'
+  # get 'catalogs/create'
+  # get 'catalogs/edit'
+  # get 'catalogs/update'
+  # get 'catalogs/destroy'
+  resources :catalogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
